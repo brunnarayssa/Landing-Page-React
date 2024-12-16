ac5437305db6f333dde9cca2e179082da44dab36
Brunna Carvalho <114610031+brunnarayssa@users.noreply.github.com>
2024-12-16 19:23:58 +0000

    actualizandocard.jsx

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -1,21 +1,85 @@
 import React from "react";
 
-
 const Card = () => {
     return (
         <>
-            <div className="card" style={{ width: "18rem" }}>
-                <img src="https://picsum.photos/200/200" className="card-img-top" alt="..." />
-                <div className="card-body">
-                    <h5 className="card-title d-flex justify-content-center">Card Title</h5>
-                    <p className="card-text d-flex justify-content-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quam mollitia unde dolor ullam dicta, eum fugiat.</p>
-                    <a href="#" className="btn btn-primary d-flex justify-content-center">Go somewhere</a>
+            <div className="container mt-5">
+                <div className="d-flex flex-wrap justify-content-between">
+                    {/* Card 1 */}
+                    <div className="card mb-4" style={{ width: "18rem" }}>
+                        <img src="https://picsum.photos/200/200" className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title d-flex justify-content-center">Card Title 1</h5>
+                            <p className="card-text d-flex justify-content-center">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            </p>
+                            <a href="#" className="btn btn-primary d-flex justify-content-center">Go somewhere</a>
+                        </div>
+                    </div>
+
+                    {/* Card 2 */}
+                    <div className="card mb-4" style={{ width: "18rem" }}>
+                        <img src="https://picsum.photos/200/200" className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title d-flex justify-content-center">Card Title 2</h5>
+                            <p className="card-text d-flex justify-content-center">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            </p>
+                            <a href="#" className="btn btn-primary d-flex justify-content-center">Go somewhere</a>
+                        </div>
+                    </div>
+
+                    {/* Card 3 */}
+                    <div className="card mb-4" style={{ width: "18rem" }}>
+                        <img src="https://picsum.photos/200/200" className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title d-flex justify-content-center">Card Title 3</h5>
+                            <p className="card-text d-flex justify-content-center">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            </p>
+                            <a href="#" className="btn btn-primary d-flex justify-content-center">Go somewhere</a>
+                        </div>
+                    </div>
+
+                    {/* Card 4 */}
+                    <div className="card mb-4" style={{ width: "18rem" }}>
+                        <img src="https://picsum.photos/200/200" className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title d-flex justify-content-center">Card Title 4</h5>
+                            <p className="card-text d-flex justify-content-center">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            </p>
+                            <a href="#" className="btn btn-primary d-flex justify-content-center">Go somewhere</a>
+                        </div>
+                    </div>
+
+                    {/* Card 5 */}
+                    <div className="card mb-4" style={{ width: "18rem" }}>
+                        <img src="https://picsum.photos/200/200" className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title d-flex justify-content-center">Card Title 5</h5>
+                            <p className="card-text d-flex justify-content-center">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            </p>
+                            <a href="#" className="btn btn-primary d-flex justify-content-center">Go somewhere</a>
+                        </div>
+                    </div>
+
+                    {/* Card 6 */}
+                    <div className="card mb-4" style={{ width: "18rem" }}>
+                        <img src="https://picsum.photos/200/200" className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title d-flex justify-content-center">Card Title 6</h5>
+                            <p className="card-text d-flex justify-content-center">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            </p>
+                            <a href="#" className="btn btn-primary d-flex justify-content-center">Go somewhere</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
     );
-}
-
-
+};
 
 export default Card;
